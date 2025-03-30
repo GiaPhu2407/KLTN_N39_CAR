836bde0a094325d5608ae8911950820e70afefc0
GiaPhu2407 <giaphudiuhien@gmail.com>
2025-03-30 19:53:15 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+const { Config } = require("tailwindcss");
+const { withUt } = require("uploadthing/tw");
+
+module.exports = withUt({
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{ts,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        // Các phần còn lại của theme
+      },
+      // Các phần mở rộng khác của theme
+    },
   },
-  plugins: [],
-};
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  plugins: [require("daisyui"), require("tailwindcss-animate")],
+});
