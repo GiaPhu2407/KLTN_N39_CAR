e11b426575c75bbcb818d84dd1aa16794e633c35
GiaPhu2407 <giaphudiuhien@gmail.com>
2025-03-31 16:19:01 +0700

    Trang Chu

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,3 @@
  // import CozeChat from "./components/CozeAi";
   
   
-import TechnicalSpecs from "./component/ui/Thongso";
-import CarViewerPage from "./component/ui/Car360";
-import Itemslide from "./component/ui/ItemSlide";
-import Footer from "./component/ui/Footer";
-import Information from "./component/ui/Information";
-import Videobg from "./component/ui/Videobg";
-import Product from "./component/ui/Product";
-import BackgroundSlider from "./component/ui/BackgroundSlide";
- 
-
-
-
-export default function Home() {
-  return (
-    <div >
-      <BackgroundSlider></BackgroundSlider>
-      <Product></Product>
-      <CarViewerPage/>
-      <TechnicalSpecs/>
-      <Videobg></Videobg>
-      <Information></Information>
-      <Itemslide/>
-      {/* <CozeChat/> */}
-      <Footer></Footer>
-    </div>
-  );
-}
