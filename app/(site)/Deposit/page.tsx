6e7e5b1441883c748162edb042999bfe334d7175
thinhkodunghang <daanhthinh@gmail.com>
2025-05-08 14:55:19 +0700

    fix payment

--- app/(site)/Deposit/page.tsx
+++ app/(site)/Deposit/page.tsx
@@ -503,35 +503,17 @@ const CarDepositPage = () => {
                 </div>
                 
                 <div className="form-control flex-1">
-                  <label className="label">
+                <label className="label">
                     <span>Giờ hẹn lấy xe</span>
                   </label>
-                  
-                  <select
-                      name="GioHenLayXe"
-                      value={pickupSchedule.GioHenLayXe}
-                      onChange={handlePickupScheduleChange}
-                      className="input input-bordered  w-full"
-                      required
-                    >
-                      <option value="">Chọn giờ hẹn</option>
-                      <option value="08:30">08:30 AM</option>
-                      <option value="09:00">09:00 AM</option>
-                      <option value="09:30">09:30 AM</option>
-                      <option value="10:00">10:00 AM</option>
-                      <option value="10:30">10:30 AM</option>
-                      <option value="11:00">11:00 AM</option>
-                      <option value="11:30">11:30 AM</option>
-                      <option value="13:30">1:30 PM</option>
-                      <option value="14:00">2:00 PM</option>
-                      <option value="14:30">2:30 PM</option>
-                      <option value="15:00">3:00 PM</option>
-                      <option value="15:30">3:30 PM</option>
-                      <option value="16:00">4:00 PM</option>
-                      <option value="16:30">4:30 PM</option>
-                      <option value="17:00">5:00 PM</option>
-                      <option value="17:30">5:30 PM</option>
-                    </select>
+                  <input
+                    type="time"
+                    name="GioHenLayXe"
+                    value={pickupSchedule.GioHenLayXe}
+                    onChange={handlePickupScheduleChange}
+                    className="input input-bordered  w-full"
+                    required
+                  />
                 </div>
               </div>
 
